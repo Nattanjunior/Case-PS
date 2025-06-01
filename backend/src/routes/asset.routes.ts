34d898fdf5df4018d8be10b5ec53a435d5d91759
nattanjunior <fnatanieljunior@gmail.com>
2025-06-01 17:51:27 -0300

    feat: implementação das rotas de assets

--- backend/src/routes/asset.routes.ts
+++ backend/src/routes/asset.routes.ts
@@ -3,4 +3,8 @@ import * as assetController from '../controllers/asset.controller'
 
 export async function assetRoutes(app: FastifyInstance) {
   app.get('/assets', assetController.list)
+  app.get('/clients/:clientId/assets', assetController.listByClient)
+  app.post('/clients/:clientId/assets', assetController.create)
+  app.put('/clients/:clientId/assets/:assetId', assetController.update)
+  app.delete('/clients/:clientId/assets/:assetId', assetController.remove)
 }
