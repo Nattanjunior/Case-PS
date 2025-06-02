87f148d6d4bf0b63c09eed2a395981c73fca870d
nattanjunior <fnatanieljunior@gmail.com>
2025-06-02 18:59:15 -0300

    feat(backend): Register allocation routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
-
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { clientRoutes } from './routes/client.routes'
 import { assetRoutes } from './routes/asset.routes'
+import { allocationRoutes } from './routes/allocation.routes'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -12,6 +12,7 @@ export const app = Fastify()
 app.register(cors)
 app.register(clientRoutes)
 app.register(assetRoutes)
+app.register(allocationRoutes)
 
 
 const PORT = process.env.PORT || 3333
