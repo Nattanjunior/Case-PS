28cd48e10e1087434075d71d6abd66d96a25deb8
nattanjunior <fnatanieljunior@gmail.com>
2025-06-02 18:59:14 -0300

    refactor(backend): Remove obsolete asset routes

--- backend/src/routes/asset.routes.ts
+++ backend/src/routes/asset.routes.ts
@@ -3,8 +3,4 @@ import * as assetController from '../controllers/asset.controller'
 
 export async function assetRoutes(app: FastifyInstance) {
   app.get('/assets', assetController.list)
-  app.get('/clients/:clientId/assets', assetController.listByClient)
-  app.post('/clients/:clientId/assets', assetController.create)
-  app.put('/clients/:clientId/assets/:assetId', assetController.update)
-  app.delete('/clients/:clientId/assets/:assetId', assetController.remove)
 }
